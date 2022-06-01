c657c47592fd04ce39cae7ff22eeb218cf25275a
Teddy Mwai <teddymwai11@gmail.com>
2022-06-02 00:48:45 +0300

    OurWork page animations

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -6,7 +6,14 @@ import theracer from "../img/theracer-small.png";
 import goodtimes from "../img/goodtimes-small.png";
 // Animations
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import {
+  pageAnimation,
+  fade,
+  photoAnim,
+  lineAnim,
+  slider,
+  sliderContainer,
+} from "../animation";
 
 const OurWork = () => {
   return (
@@ -17,11 +24,19 @@ const OurWork = () => {
       exit="exit"
       style={{ background: "#fff" }}
     >
+      <motion.div variants={sliderContainer}>
+        <Frame1 variants={slider}></Frame1>
+        <Frame2 variants={slider}></Frame2>
+        <Frame3 variants={slider}></Frame3>
+        <Frame4 variants={slider}></Frame4>
+      </motion.div>
       <Movie>
-        <h2>The Athlete</h2>
-        <div className="line"></div>
+        <motion.h2 variants={fade}>The Athlete</motion.h2>
+        <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/the-athlete">
-          <img src={athlete} alt="Athlete" />
+          <Hide>
+            <motion.img variants={photoAnim} src={athlete} alt="Athlete" />
+          </Hide>
         </Link>
       </Movie>
       <Movie>
@@ -54,7 +69,7 @@ const Work = styled(motion.div)`
 const Movie = styled.div`
   padding-bottom: 10rem;
   .line {
-    background: #cccccc;
+    background: #23d997;
     height: 0.5rem;
     margin-bottom: 3rem;
   }
@@ -65,4 +80,27 @@ const Movie = styled.div`
   }
 `;
 
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+// Frame Animation
+const Frame1 = styled(motion.div)`
+  position: fixed;
+  left: 0;
+  top: 10%;
+  width: 100%;
+  height: 100vh;
+  background: #fffebf;
+  z-index: 2;
+`;
+const Frame2 = styled(Frame1)`
+  background: #ff8efb;
+`;
+const Frame3 = styled(Frame1)`
+  background: #8ed2ff;
+`;
+const Frame4 = styled(Frame1)`
+  background: #8effa0;
+`;
 export default OurWork;
