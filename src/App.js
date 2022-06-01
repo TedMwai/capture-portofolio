b670e5953447c65db912716602d578c762e5df13
Teddy Mwai <teddymwai11@gmail.com>
2022-06-01 18:35:28 +0300

    added movie details

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@
 import GlobalStyle from "./components/GlobalStyle";
 //Import Pages
 import AboutUs from "./pages/AboutUs";
+import Nav from "./components/Nav";
+import OurWork from "./pages/OurWork";
+import ContactUs from "./pages/ContactUs";
+import MovieDetails from "./pages/MovieDetail";
+// Router
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
-      <AboutUs/>
+      <Nav />
+      <Routes>
+      <Route path="/" element={<AboutUs />} /> 
+      <Route path="/work" element={<OurWork />} /> 
+      <Route path="/work/:id" element={<MovieDetails />} /> 
+      <Route path="/contact" element={<ContactUs />} /> 
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/OurWork.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+// Images
+import athlete from "../img/athlete-small.png";
+import theracer from "../img/theracer-small.png";
+import goodtimes from "../img/goodtimes-small.png";
+
+const OurWork = () => {
+  return (
+    <Work>
+      <Movie>
+        <h2>The Athlete</h2>
+        <div className="line"></div>
+        <Link to="/work/the-athlete">
+          <img src={athlete} alt="Athlete" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>The Racer</h2>
+        <div className="line"></div>
+        <Link to="/work/the-racer">
+          <img src={theracer} alt="Racer" />
+        </Link>
+      </Movie>
+      <Movie>
+        <h2>Good times</h2>
+        <div className="line"></div>
+        <Link to="/work/good-times">
+          <img src={goodtimes} alt="Good Times" />
+        </Link>
+      </Movie>
+    </Work>
+  );
+};
+
+const Work = styled.div`
+  min-height: 100vh;
+  overflow: hidden;
+  padding: 5rem 10rem;
+  h2 {
+    padding: 1rem 0rem;
+  }
+`;
+
+const Movie = styled.div`
+  padding-bottom: 10rem;
+  .line {
+    background: #cccccc;
+    height: 0.5rem;
+    margin-bottom: 3rem;
+  }
+  img {
+    width: 100%;
+    height: 70vh;
+    object-fit: cover;
+  }
+`;
+
+export default OurWork;
